d0419b123b3164b09e7d12273fbca4a3fd70cc7f
lvgang <1547554745@qq.com>
2019-11-10 14:15:34 +0800

    first version

---
+++ script/test.py
@@ -0,0 +1,8 @@
+# a = {'bb': 'c'}
+# print('bb' in a.keys())
+
+# a = "select * from apisamplecode  LIMIT {},1".format(2)
+# import re
+#
+# sentence = "Provides the classes and interfaces of the Java<sup><font size='-2'>TM</font></sup> 2 platform's core logging facilities."
+# print(re.sub(r"</?(.+?)>", "", sentence))
